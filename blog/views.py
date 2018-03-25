ec4521fbd19fef28da9ee83ea5f697b70a0a3395
indraTeja <indrateja@gmail.com>
2018-03-25 12:54:46 -0500

    watson-changes

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+        username='358aa707-ceee-4c84-96fa-475bc7d60516',
+        password='YcL8Kwi0pF4o',
+        version='2016-05-19 ')
+
+    language_translator = LanguageTranslator(
+        username='a686258f-33d7-4c9d-87b2-dcf8818dcc10',
+        password='MNODMlo1w2tQ')
+
+    # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    for post in posts:
+        posting = post.text
+        toneObj= json.dumps(tone_analyzer.tone(tone_input=posting,
+                                   content_type="text/plain"), indent=2)
+        post.toneObj2 = json.loads(toneObj)
+        post.angerScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][0]['score']
+        post.disgustScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][1]['score']
+        post.fearScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][2]['score']
+        post.joyScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][3]['score']
+        post.sadScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][4]['score']
+
+        translation = language_translator.translate(
+            text=post.text,
+            source='en',
+            target='de')
+        obj= json.dumps(translation, indent=2, ensure_ascii=False)
+        post.obj2 = json.loads(obj)
+        post.translate_spanish = post.obj2['translations'][0]['translation']#[0]['word_count'][0]['character_count']
+        post.wordcount = post.obj2['word_count']
+        post.charactercount = post.obj2['character_count']
+
+
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-	
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-	
+
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +64,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-	
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -43,3 +79,5 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
